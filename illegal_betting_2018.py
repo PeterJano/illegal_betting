971831b2caa483876ce5bea08450be9e08eb5c89
AdamSzucs1 <adaam.szucs@gmail.com>
2018-10-25 11:23:29 +0200

    Minor updates

--- illegal_betting_2018.py
+++ illegal_betting_2018.py
@@ -2,18 +2,15 @@ import random
 
 def betting_on_horse(horses):
     betting_is_on = True
-    print(horses)
     while betting_is_on:
         try:
             bet = input("Which horse you wanna bet on? ")
-            bet = int(bet)
-            if bet > 0 and bet < 9:
-                print("jó a bemenet!!")
+            bet = str(bet)
+            if bet in horses.keys():
+                print("You bet on: " + bet)
                 return bet
-                betting_is_on = False
-                break
             else:
-                print("Try again homie, this is not the right number!")
+                print("Try again homie, this is not right!")
         except(ValueError, TypeError):
             print("This is not a number at all.")
 
@@ -38,16 +35,27 @@ def betting_cash_amount(money):
                 odds = input("What odds you wanna bet? ")
 '''
 
+def race_is_on():
+    
 
 def main():
     money = 100
     horses = {'Chop Chop': 2.0, 'Indian Sea': 3.2, 'Iriss Spirit': 4.3, 
               'Parknacilla': 1.5, 'Pytilla': 2.5, 'Rainbow Heart': 7.0, 
               'Storm Athena': 1.8, 'Tartlette': 0.7}
+    # print(f"Horses{:15} Odds{:1}")
+    print("Horses:\t\tOdds:\n")
+
+    for horse in horses.keys():
+        print(f"{horse:15} {horses[horse]:.1f}")
+
+    print("\n")
+
     while True:
         betting_on_horse(horses)
-        betting_cash_amount(money)
-
+        actual_bet = betting_cash_amount(money)
+        money -= actual_bet
+        print(money)
 
 
 main()
